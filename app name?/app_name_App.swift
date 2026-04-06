9904eee10f5a57a9f9f0f8cf244d291881029c5c
DevwasTaken-yes <setyawandevon@gmail.com>
2026-04-06 16:04:39 +0700

    Initial Commit

---
+++ app name?/app_name_App.swift
@@ -0,0 +1,17 @@
+//
+//  app_name_App.swift
+//  app name?
+//
+//  Created by Devon on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct app_name_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
